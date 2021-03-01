dec4c6c82130032b54c056a196bda15a25139e13
Gus Deuk <gusdeuk@gmail.com>
2021-03-01 16:49:26 -0300

    update filters

--- poc-hotels/src/pages/CatalogPage/CatalogFilter/CatalogFilter.vue
+++ poc-hotels/src/pages/CatalogPage/CatalogFilter/CatalogFilter.vue
@@ -1,35 +1,43 @@
 <template>
     <div class="filter-component">
         <div class="column-1">
-            <label class="form-label">Rewards customer?</label>
-            <div class="customer-block">
-                <label class="custom-label" for="customerType">NO</label>
-                <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" id="customerType" @change="toggleCustomerType" checked />
+            <div>
+                <div>
+                    <label class="form-label">Rewards customer?</label>
+                    <div class="customer-block">
+                        <label class="custom-label" for="customerType">NO</label>
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" id="customerType" @change="toggleCustomerType" checked />
+                        </div>
+                        <label class="custom-label" for="customerType">YES</label>
+                    </div>
                 </div>
-                <label class="custom-label" for="customerType">YES</label>
-            </div>
 
-            <div class="ammenities-block form-check">
-                <input class="form-check-input" type="checkbox" checked value="" id="toggleAmmenities" @change="toggleAmmenities" />
-                <label for="toggleAmmenities" class="form-label noselect">
-                    Show Ammenities
-                </label>
+                <div class="ammenities-block form-check">
+                    <input class="form-check-input" type="checkbox" checked value="" id="toggleAmmenities" @change="toggleAmmenities" />
+                    <label for="toggleAmmenities" class="form-label noselect">
+                        Show Ammenities
+                    </label>
+                </div>
             </div>
         </div>
 
         <div class="column-2">
             <div class="dates-block">
-                <label for="basic-addon1" class="form-label">Start date</label>
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="basic-addon1">@</span>
-                    <input type="date" class="form-control" placeholder="Start Date" @change="setStartDate" />
+                <div>
+                    <label for="basic-addon1" class="form-label">Start date</label>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">@</span>
+                        <input type="date" class="form-control" placeholder="Start Date" @change="setStartDate" />
+                    </div>
                 </div>
 
-                <label for="basic-addon1" class="form-label">End date</label>
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="basic-addon1">@</span>
-                    <input type="date" class="form-control" placeholder="End Date" @change="setEndDate" />
+                <div>
+                    <label for="basic-addon1" class="form-label">End date</label>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">@</span>
+                        <input type="date" class="form-control" placeholder="End Date" @change="setEndDate" />
+                    </div>
                 </div>
             </div>
         </div>
